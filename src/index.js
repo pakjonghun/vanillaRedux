d2f34e3ea02df9509c2860e5abbf4406ed65a078
정훈박 <fireking5997@gmail.com>
2021-12-03 22:36:27 +0900

    Add:Todo App

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 import { createStore } from "redux";
+import shortid from "shortid";
 
-const value = document.getElementById("value");
-const plus = document.getElementById("plus");
-const minus = document.getElementById("minus");
+const ul = document.getElementById("ul");
+const form = document.getElementById("form");
+const input = document.getElementById("input");
 
-const INCREASE = "INCREASE";
-const DECREASE = "DECREASE";
+const ADD = "ADD";
+const DONE = "DONE";
+const DELETE = "DELETE";
 
-//리듀서만 스테이트를 관리한다!
-//1. 처음에는 state=default 가 action type init으로 불려진다.
-//2. 그 다음에는 내가 바꾸기 나름!!
-//3. action 은 obj 여야 한다.
-const reducer = (state = 0, action) => {
-  switch (action.type) {
-    case INCREASE:
-      return state + 1;
+const reducer = (state = [], { text, type, id, isDone }) => {
+  switch (type) {
+    case ADD:
+      return [{ text, id: shortid.generate(), isDone: 0 }, ...state];
 
-    case DECREASE:
-      return state - 1;
-
+    case DONE:
+      const newState = state.map((item) => {
+        if (item.id === id) {
+          return { ...item, isDone: !+isDone ? 1 : 0 };
+        }
+        return item;
+      });
+      return newState;
+    case DELETE:
+      return state.filter((item) => item.id !== id);
     default:
       return state;
   }
@@ -26,16 +31,22 @@ const reducer = (state = 0, action) => {
 
 const store = createStore(reducer);
 
-const render = () => {
-  value.innerText = store.getState();
-};
+const { dispatch, subscribe, getState } = store;
+window.dispatch = dispatch;
+function render() {
+  const tags = getState().map(({ text, id, isDone }) => {
+    return `<li class='${isDone ? "red" : ""}'
+              onClick={dispatch({type:"${DONE}",id:"${id}",isDone:"${isDone}"})}>${text}
+              <button onClick={dispatch({type:"${DELETE}",id:"${id}"})}>del</button>
+            </li>`;
+  });
 
-//subscribe 는 리듀서 호출시 어떤 일을 한다.(여기서는 render)
-store.subscribe(render);
-
-function plusFunc() {
-  //dispatch를 통해서 reducer 에 소통한다.
-  store.dispatch({ type: INCREASE });
+  ul.innerHTML = tags.join("");
 }
 
-plus.addEventListener("click", plusFunc);
+subscribe(render);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  dispatch({ type: ADD, text: input.value });
+});
